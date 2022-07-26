6e95a2106b1dfcaf602854bddc566fecebe1ae5b
jerednav <80506833+jerednav@users.noreply.github.com>
2022-07-26 08:12:03 -0700

    Update and rename 1-uncompress to 1-uncompress.js

--- ArrayAndStrings/1-uncompress.js
+++ ArrayAndStrings/1-uncompress.js
@@ -1,4 +1,4 @@
--Prompt
+# Prompt
 
 Write a function, uncompress, that takes in a string as an argument. The input string will be formatted into multiple groups according to the following pattern:
 
