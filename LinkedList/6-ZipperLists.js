0af6328b7fa147d5f1b8b76da22c759cc3565b40
jerednav <80506833+jerednav@users.noreply.github.com>
2022-08-24 10:03:03 -0700

    Update 6-ZipperLists.js

--- LinkedList/6-ZipperLists.js
+++ LinkedList/6-ZipperLists.js
@@ -99,4 +99,38 @@ zipperLists(one, w);
 
 
 //Approach
-1. Create current 
+1. Create current1 & 2 variables
+2. Since head1 will be the head1, assign it to head1
+3. let tail be equal to the head so we can add the values from head1 and head2
+4. add a count to decide which linked list to pull from
+5. while loop if current1 & current2 is NOT null
+6. if loop, if count is evenmake tail.next equal to current2, and pass the current2 to current2.next
+7. else,  (count % 2), make tail.next equal to current1, and pass the current2 to current1.next
+8. after, increment the count up one, and assign the new tail to tail.next
+9. Once either current1 or current 2 is NOT nullreturn the rest of either current 1 or 2
+10. return the head which will consist of the zippered list.
+
+//ITERATIVE Solution
+const zipperLists = (head1, head2) => {
+  let head = head1
+  let current1= head1.next
+  let current2 = head2
+  let tail = head
+  let count = 0
+  
+  while(current1 !== null && current2 !== null){
+    if (count % 2 === 0){
+      tail.next = current2
+      current2 = current2.next
+    } else {
+      tail.next = current1
+      current1 = current1.next
+    }
+    count++
+    tail = tail.next
+  }
+  if (current1 !== null) tail.next = current1
+  if (current2 !== null) tail.next = current2
+  
+  return head
+};
