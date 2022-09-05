993ebd32e7958c2470da255af7c18aebf7ccfe29
jerednav <80506833+jerednav@users.noreply.github.com>
2022-09-05 08:20:26 -0700

    Update 13-AddLists.js

--- LinkedList/13-AddLists.js
+++ LinkedList/13-AddLists.js
@@ -124,4 +124,57 @@ const b1 = new Node(6);
 addLists(a1, b1);
 // 5 -> 0 -> 0 -> 1
 
+//Starting 
+const addLists = (head1, head2) => {
+
+};
+
+
+
 //Recursive solution
+const addLists = (head1, head2, carry = 0) => {
+ if (head1 === null && head2 === null && carry === 0) return null
+  
+  const val1 = head1 === null ? 0 : head1.val
+  const val2 = head2 === null ? 0 : head2.val
+  
+  const sum = val1 + val2 + carry
+  const nextCarry = sum > 9 ? 1 : 0
+  const digit = sum % 10
+  
+  const resultNode = new Node(digit)
+  
+  const next1 = head1 === null ? null : head1.next
+  const next2 = head2 === null ? null : head2.next
+  
+  resultNode.next = addLists(next1, next2, nextCarry)
+  return resultNode
+};
+
+
+
+//Iterative solution
+const addLists = (head1, head2) => {
+  const dummyHead = new Node(null)
+  let tail = dummyHead
+  
+  let carry = 0
+  let current1 = head1
+  let current2 = head2
+  
+  while (current1 !== null || current2 !== null || carry !== 0){
+      const val1 = current1 === null ? 0 : current1.val
+      const val2 = current2 === null ? 0 : current2.val
+      const sum = val1 + val2 + carry
+      carry = sum > 9 ? 1 : 0;
+    
+      const digit = sum % 10
+      tail.next = new Node(digit)
+      tail = tail.next
+    
+      if (current1 !== null) current1 = current1.next
+      if (current2 !== null) current2 = current2.next
+  }
+  
+  return dummyHead.next
+};
