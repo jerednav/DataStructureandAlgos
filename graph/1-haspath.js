e806371221b113bcf2f3ed71186220d9f235ba37
jerednav <80506833+jerednav@users.noreply.github.com>
2022-10-04 08:11:54 -0700

    Update 1-haspath.js

--- graph/1-haspath.js
+++ graph/1-haspath.js
@@ -66,3 +66,32 @@ hasPath(graph, 'v', 'z'); // false
 
 
 
+//recursive 
+const hasPath = (graph, src, dst) => {
+  if (src === dst) return true
+  
+  for (let neighbor of graph[src]){
+    if (hasPath(graph, neighbor, dst) === true){
+      return true
+    }
+  }
+  return false
+};
+
+
+
+//iterative
+const hasPath = (graph, src, dst) => {
+ const queue = [src]
+ 
+ while(queue.length > 0){
+   const current = queue.shift()
+   
+   if(current === dst) return true
+   
+   for(let neighbor of graph[current]){
+     queue.push(neighbor)
+   }
+ }
+  return false
+};
