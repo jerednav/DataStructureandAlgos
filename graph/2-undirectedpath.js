2b6c528eb4da8559bb2869d5053b7f7c5d1fac5f
jerednav <80506833+jerednav@users.noreply.github.com>
2022-10-12 07:52:14 -0700

    Update and rename 2-undirectedpath.j;s to 2-undirectedpath.js

--- graph/2-undirectedpath.js
+++ graph/2-undirectedpath.js
@@ -127,3 +127,21 @@ const edges = [
 undirectedPath(edges, 'r', 't'); // -> true
 
 
+const hasPath = (graph, src, dst) => {
+ const queue = [src]
+ 
+ while(queue.length > 0){
+   const current = queue.shift()
+   
+   if(current === dst) return true
+   
+   for(let neighbor of graph[current]){
+     queue.push(neighbor)
+   }
+ }
+  return false
+};
+
+module.exports = {
+  hasPath,
+};
